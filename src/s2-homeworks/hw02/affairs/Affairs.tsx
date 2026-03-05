581e17db0800644f231c73cc07f596ab3f4278ff
MaratMN <breakboymarty@gmail.com>
2026-03-06 01:48:06 +0300

    try to complete HW2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,24 +4,27 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[]// need to fix any
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (id: AffairType["_id"])=> void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
-    const setAll = () => {
+    const setAll = () => {props.setFilter("all")
         // need to fix
     }
     const setHigh = () => {
+        props.setFilter("high")
         // need to fix
     }
     const setMiddle = () => {
         // need to fix
+        props.setFilter("middle")
     }
     const setLow = () => {
         // need to fix
+        props.setFilter("low")
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
