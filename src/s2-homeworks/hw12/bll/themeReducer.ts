8e7a338dc7350236d04a3f62af71a07a7d78a9ca
MaratMN <breakboymarty@gmail.com>
2026-05-17 12:08:00 +0300

    completedHW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,9 +1,22 @@
+
+type initStateType = {
+    themeId: number,
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type changeThemeIdActionType = {
+    type: 'SET_THEME_ID',
+    id: number
+
+}
+
+export const themeReducer = (state = initState, action: changeThemeIdActionType): initStateType => { // fix any
     switch (action.type) {
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
         // дописать
 
         default:
@@ -11,4 +24,4 @@ export const themeReducer = (state = initState, action: any): any => { // fix an
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeIdActionType => ({ type: 'SET_THEME_ID', id }) // fix any
